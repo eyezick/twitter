3ac16d41e207babe033bf8f4895ff4b103e051e1
Isaac Ibiapina <isaac1@mail.usf.edu>
2017-03-07 10:29:57 -0500

    welcome added

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(request, response){
+    response.send('WELCOME BRETHREN!)
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log('i like big booties')
+})
